80a0bad4a8d5350cbfa46553df989aa7eafaac53
RohitSah23 <rohitisog@gmail.com>
2025-09-09 19:05:05 +0530

    feat (Header):Header added

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@ import Image from "next/image";
 
 import HeroSection from "@/components/hero";
 import FooterSection from "@/components/footer";
+import HeaderSection from "@/components/header";
 
 export default function Home() {
   return (
   <div>
+    <HeaderSection />
     <HeroSection />
     <FooterSection />
   </div>
